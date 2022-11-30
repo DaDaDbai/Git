8824418103d4f665e378d1da377395ed54d95d3b
lucf <1350501758@qq.com>
2022-11-30 21:17:48 +0800

    SEC commit

--- src/main/java/cn/lcf/Git/Test.java
+++ src/main/java/cn/lcf/Git/Test.java
@@ -3,5 +3,7 @@ package cn.lcf.Git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("kkk");
+        System.out.println("kkk");
+        System.out.println("kkk");
     }
 }
